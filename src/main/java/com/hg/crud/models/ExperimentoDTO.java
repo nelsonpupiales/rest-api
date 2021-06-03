296abae837f389d3465092094949e6fa565d13dd
Nelson Pupiales <nelsonpupiales@gmail.com>
2021-06-03 11:57:40 -0500

    Carga Experimento

--- src/main/java/com/hg/crud/models/ExperimentoDTO.java
+++ src/main/java/com/hg/crud/models/ExperimentoDTO.java
@@ -5,10 +5,73 @@
  */
 package com.hg.crud.models;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 /**
  *
  * @author nelso
  */
+
+@Document(collection = "experimentos")
+
 public class ExperimentoDTO {
+    
+    @Id
+    private String id;
+    private String nombreExperimento;
+    private String preguntaExperimento;
+    private String dataExperimento;
+    private String labelExperimento;
+    private String idTema;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombreExperimento() {
+        return nombreExperimento;
+    }
+
+    public void setNombreExperimento(String nombreExperimento) {
+        this.nombreExperimento = nombreExperimento;
+    }
+
+    public String getPreguntaExperimento() {
+        return preguntaExperimento;
+    }
+
+    public void setPreguntaExperimento(String preguntaExperimento) {
+        this.preguntaExperimento = preguntaExperimento;
+    }
+
+    public String getDataExperimento() {
+        return dataExperimento;
+    }
+
+    public void setDataExperimento(String dataExperimento) {
+        this.dataExperimento = dataExperimento;
+    }
+
+    public String getLabelExperimento() {
+        return labelExperimento;
+    }
+
+    public void setLabelExperimento(String labelExperimento) {
+        this.labelExperimento = labelExperimento;
+    }
+
+    public String getIdTema() {
+        return idTema;
+    }
+
+    public void setIdTema(String idTema) {
+        this.idTema = idTema;
+    }
+    
     
 }
