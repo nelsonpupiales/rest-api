e1bb5a427d5e91da17e0cf1e6b2d93a9b7d93102
Nelson Pupiales <nelsonpupiales@gmail.com>
2021-06-03 18:03:39 -0500

    Carga Materia Estudiante

--- src/main/java/com/hg/crud/models/MateriaEstudianteDTO.java
+++ src/main/java/com/hg/crud/models/MateriaEstudianteDTO.java
@@ -5,10 +5,45 @@
  */
 package com.hg.crud.models;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 /**
  *
- * @author nelso
+ * @author nelson pupiales
  */
+
+@Document(collection = "materiasestudiantes")
+
 public class MateriaEstudianteDTO {
+    @Id
+    private String id;
+    private String idMateria;
+    private String idEstudiante;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(String idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public String getIdEstudiante() {
+        return idEstudiante;
+    }
+
+    public void setIdEstudiante(String idEstudiante) {
+        this.idEstudiante = idEstudiante;
+    }
+    
     
 }
